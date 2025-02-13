641e16196a1369eb04831f8accd6767b2e66c79f
saladchalice <edwardlunited@gmail.com>
2025-02-12 23:10:28 -0800

    lab 06 finished

--- global.js
+++ global.js
@@ -21,6 +21,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'resume/', title: 'Resume' },
   { url: 'contact/', title: 'Contact' },
+  { url: 'meta/', title: 'Meta' },
   { url: 'https://github.com/saladchalice/shrkshkk', title: 'Github' }
 ];
 
